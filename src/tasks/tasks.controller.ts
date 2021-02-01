8fe2b62989a6c8d5dcc694ebed6ca88bfaadec73
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-02-01 09:23:21 -0300

    Adding Create, Update and Delete on Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Task } from './shared/task';
 import { TaskService } from './shared/task.service';
 
@@ -14,5 +22,21 @@ export class TasksController {
   @Get(':id')
   async getById(@Param('id') id: number): Promise<Task> {
     return this.taskService.getById(id);
+  }
+
+  @Post()
+  async create(@Body() task: Task): Promise<Task> {
+    return this.taskService.create(task);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+    task.id = id;
+    return this.taskService.update(task);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.taskService.delete(id);
   }
 }
